a51593b05850b6b17981a50f57419c4054d0af0f
Ernie Turner <ernie.turner@ironcorelabs.com>
2017-09-25 14:49:10 -0600

    Fix type generation to avoid promise error and generate types in index.d.ts since otherwise karma-typescript complains

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,4 @@
 module.exports = function(config) {
-    const fileListGlob = "Future.ts";
-    //Allow for filtering down files to run to just match the item passed in
-    let clientArgs = {};
-    if(config.filter){
-        clientArgs = {
-            args: ['--grep', config.filter]
-        };
-    }
-
     config.set({
         frameworks: ["jasmine", "karma-typescript", "jasmine-matchers"],
         files: [
@@ -21,14 +12,13 @@ module.exports = function(config) {
             suppressSkipped: true,
             showSpecTiming: true
         },
-        client: clientArgs,
         browserNoActivityTimeout: 20000,
         browsers: ["ChromeHeadless"],
         singleRun: true,
         karmaTypescriptConfig: {
             compilerOptions: {
                 target: "es6",
-            }
+            },
         }
     });
 };
